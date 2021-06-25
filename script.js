ccc6118616afd2aba5ff46a771aee164050c24a7
cdalangin <florenzdalangin@gmail.com>
2021-06-25 13:10:55 -0400

    test login/signup page

---
+++ script.js
@@ -0,0 +1,17 @@
+var x = document.getElementById("login");
+var y = document.getElementById("signup");
+
+function createAcct() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("signup");
+    x.style.display = "none";
+    y.style.display = "block";
+
+}
+
+function enterAcct() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("signup");
+    x.style.display = "block";
+    y.style.display = "none";
+}
